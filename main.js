6121c213ef37a15bf850e7614f32dddaa0effcde
Nutty-Nuts <vegagerard1130@gmail.com>
2022-05-28 21:50:38 +0800

    finished rps functionality

--- main.js
+++ main.js
@@ -32,6 +32,9 @@ function playRound(event) {
     const matchHistory = document.querySelector("#match-history");
     const createLog = document.createElement("p");
 
+    const player = document.querySelector(".player");
+    const computer = document.querySelector(".computer");
+
     console.log(
         `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection}`
     );
@@ -45,36 +48,34 @@ function playRound(event) {
         createLog.textContent = `${roundNumber}: Player Wins`;
         playerScore++;
         matchHistory.appendChild(createLog);
-
-        return 0;
+        player.textContent = `Player: ${playerScore}`;
     } else if ((playerSelection = computerSelection)) {
         console.log("Draw");
         createLog.textContent = `${roundNumber}: Draw`;
         matchHistory.appendChild(createLog);
-
-        return 1;
     } else {
         console.log("Computer Wins");
         createLog.textContent = `${roundNumber}: Computer Wins`;
         computerScore++;
         matchHistory.appendChild(createLog);
-
-        return 2;
+        computer.textContent = `Computer: ${computerScore}`;
     }
+
+    gameWinLogic(playerScore, computerScore);
 }
 
 var gameWinLogic = (playerScore, computerScore) => {
-    const winner = document.querySelector(".winner");
-    const whoWins = document.querySelector("p");
+    const winner = document.querySelector("#winner");
+    const whoWins = document.createElement("p");
     if (playerScore == 5 || computerScore == 5) {
         if (playerScore > computerScore) {
-            whoWins.textContent = "Player Wins";
+            whoWins.textContent = "Player Wins the Game";
             winner.appendChild(whoWins);
-            console.log("Player Wins");
+            console.log("Player Wins the Game");
         } else if (computerScore > playerScore) {
-            whoWins.textContent = "Computer Wins";
+            whoWins.textContent = "Computer Wins the Game";
             winner.appendChild(whoWins);
-            console.log("Computer Wins");
+            console.log("Computer Wins the Game");
         } else {
             console.log("Draw");
         }
