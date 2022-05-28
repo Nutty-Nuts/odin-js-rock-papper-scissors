1bba02de5ea0472982c4fa03290647c91bc1fead
Nutty-Nuts <vegagerard1130@gmail.com>
2022-05-28 21:42:44 +0800

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const button = document.querySelector("#select-button");
+button.addEventListener("click", playRound);
+
+var roundNumber = 0;
+var playerScore = 0;
+var computerScore = 0;
+
 var computerPlay = () => {
     var numGen = Math.floor(Math.random() * 3);
 
@@ -14,58 +21,63 @@ var translateSelection = (playerSelection) => {
     }
 };
 
-var playRound = (playerSelection, computerSelection) => {
-    playerSelection = translateSelection(playerSelection);
+function playRound(event) {
+    roundNumber++;
+
+    const { select } = event.target.dataset;
+    playerSelection = select;
+
+    computerSelection = computerPlay();
+
+    const matchHistory = document.querySelector("#match-history");
+    const createLog = document.createElement("p");
 
     console.log(
         `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection}`
     );
 
     if (
-        (playerSelection == 0 && computerSelection == 2) ||
-        (playerSelection == 1 && computerSelection == 0) ||
-        (playerSelection == 2 && computerSelection == 1)
+        (playerSelection == "0" && computerSelection == "2") ||
+        (playerSelection == "1" && computerSelection == "0") ||
+        (playerSelection == "2" && computerSelection == "1")
     ) {
+        console.log("Player Wins");
+        createLog.textContent = `${roundNumber}: Player Wins`;
+        playerScore++;
+        matchHistory.appendChild(createLog);
+
         return 0;
-    } else if (playerSelection == computerSelection) {
+    } else if ((playerSelection = computerSelection)) {
+        console.log("Draw");
+        createLog.textContent = `${roundNumber}: Draw`;
+        matchHistory.appendChild(createLog);
+
         return 1;
     } else {
+        console.log("Computer Wins");
+        createLog.textContent = `${roundNumber}: Computer Wins`;
+        computerScore++;
+        matchHistory.appendChild(createLog);
+
         return 2;
     }
-};
+}
 
-var gameWinLogic = (player, computer) => {
-    if (player > computer) {
-        console.log("Player Wins");
-    } else if (computer > player) {
-        console.log("Computer Wins");
-    } else {
-        console.log("Draw");
-    }
-};
-
-var gameStart = () => {
-    var player = 0;
-    var computer = 0;
-
-    for (let i = 0; i < 5; i++) {
-        var playerSelection = prompt("Rock, Paper, Scissors...");
-        var computerSelection = computerPlay();
-
-        var winState = playRound(playerSelection, computerSelection);
-
-        if (winState == 0) {
-            player++;
-        } else if (winState == 2) {
-            computer++;
+var gameWinLogic = (playerScore, computerScore) => {
+    const winner = document.querySelector(".winner");
+    const whoWins = document.querySelector("p");
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore > computerScore) {
+            whoWins.textContent = "Player Wins";
+            winner.appendChild(whoWins);
+            console.log("Player Wins");
+        } else if (computerScore > playerScore) {
+            whoWins.textContent = "Computer Wins";
+            winner.appendChild(whoWins);
+            console.log("Computer Wins");
         } else {
-            // Nothing Happens
+            console.log("Draw");
         }
-
-        console.log(`Player — ${player} : Computer — ${computer}`);
+    } else {
     }
-
-    gameWinLogic(player, computer);
 };
-
-gameStart();
